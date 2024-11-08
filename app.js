d11a8b11de57cbfdc42c0f132a9e61559db0493f
Roshan <roshan.cheeyapana.com>
2024-11-08 16:22:37 +0530

    NODE_ENV_KEY

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 GEREKLİ PAKETLER YÜKLENİYOR...
 
 */
+require('dotenv').config()
 var http = require('http');
 var express = require('express');
-
 var app = express();
 
 app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
 app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
 app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
 app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
-
+console.log(process.env.NODE_ENV_KEY)
 require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
 
 /*
@@ -20,6 +20,7 @@ require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
 HTTP SERVER OLUŞTURULDU
 
 */
+const  env= process.env.NODE_ENV_KEY
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+	console.log('Sistem ' + app.get('port') + ` ${env}`);
 });
